d32cdaedccfab5c210dd0aab3a7c773d0cb50207
Alison Childers <alisonchilders314@gmail.com>
2022-12-11 13:08:18 -0600

    initial commit

---
+++ src/firebase-config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC5aFsjKiuGgD-3uxDtO50vuOjWujZEds4",
+    authDomain: "disney-daily-planner-dbad2.firebaseapp.com",
+    databaseURL: "https://disney-daily-planner-dbad2-default-rtdb.firebaseio.com",
+    projectId: "disney-daily-planner-dbad2",
+    storageBucket: "disney-daily-planner-dbad2.appspot.com",
+    messagingSenderId: "401303154540",
+    appId: "1:401303154540:web:377823426934ae833fcd49",
+    measurementId: "G-T55ZHSVGQY"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
