c98b812ec1071221f3bfee88739fcac1133d3bd6
Alison Childers <alisonchilders314@gmail.com>
2022-12-11 19:50:46 -0600

    firebase & react hooked up, basic crud

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { db } from './firebase-config'
+import { collection, getDocs, addDoc, deleteDoc, doc } from 'firebase/firestore'
 
 function App() {
+  const [newEmail, setNewEmail] = useState("")
+  const [newName, setNewName] = useState("")
+  const [users, setUsers] = useState([])
+  const usersCollectionRef = collection(db, "users")
+
+  const createUser = async () => {
+    let newUser = {email: newEmail, firstName: newName}
+    await addDoc(usersCollectionRef, newUser)
+    setUsers([...users, newUser])
+  }
+
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, "users", id)
+    await deleteDoc(userDoc)
+  }
+
+  useEffect(() => {
+
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef)
+      setUsers(data.docs.map((doc) =>({...doc.data(), id: doc.id})));
+    }
+
+    getUsers()
+  })
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="email" placeholder="Email..." onChange={(e) => {setNewEmail(e.target.value)}}/>
+      <input placeholder="First Name..." onChange={(e) => {setNewName(e.target.value)}}/>
+      <button onClick={createUser}>Create User</button>
+
+      {users.map((user) => {
+        return (
+        <div key={user.id}>
+          <h1>Email: {user.email}</h1>
+          <h2>First Name: {user.firstName}</h2>
+          <button onClick={() => {deleteUser(user.id)}}>Delete {user.firstName}</button>
+        </div>
+        );
+      })}
     </div>
   );
 }
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,13 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from '@firebase/firestore'
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
-    apiKey: "AIzaSyC5aFsjKiuGgD-3uxDtO50vuOjWujZEds4",
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
     authDomain: "disney-daily-planner-dbad2.firebaseapp.com",
     databaseURL: "https://disney-daily-planner-dbad2-default-rtdb.firebaseio.com",
     projectId: "disney-daily-planner-dbad2",
@@ -20,4 +23,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
+
+export const db = getFirestore(app)
 
