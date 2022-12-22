793dea3fc07cdb0db5fabc9bfba5ac3319c78b8f
Alison Childers <alisonchilders314@gmail.com>
2022-12-22 15:07:23 -0600

    fixed profile dropdown issue

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,27 +4,27 @@ import React from "react";
 function Nav(){
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-            <div className="container-fluid">
                 <Link className="nav-link logo" to="/"><i class="fa-solid fa-suitcase-rolling"></i></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/">Disney Daily Planner</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/my-plans">Attractions</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/faq">FAQ</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/contact">Contact Us</Link>
-                    </li>
-                </ul>
-                </div>
+                    <div className="container-fluid">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Disney Daily Planner</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/my-plans">Attractions</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/faq">FAQ</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/contact">Contact Us</Link>
+                            </li>
+                        </ul>
+                    </div>
                 <li className="nav-item dropdown">
                     <Link className="nav-link nav-item dropdown-toggle" to="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="fa-solid fa-user"></i>  Alison's Profile
